fc533b76358f400aaf6401704fbe48cf9be73785
Richard Lee <r7568436@gmail.com>
2024-06-14 11:58:39 +0800

    Add project files.

---
+++ MVCToDoListApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCToDoListApp.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
